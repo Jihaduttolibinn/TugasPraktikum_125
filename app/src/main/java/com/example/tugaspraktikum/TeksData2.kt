11a9c66adcb5af677b3d3f67f28f735ddc1de4ab
jihaduttolibinn <jihaduttolibin02@gmail.com>
2025-10-31 08:58:47 +0700

    Menambahkan namaInput di bagian funFormDataDiriStyled

--- app/src/main/java/com/example/tugaspraktikum/TeksData2.kt
+++ app/src/main/java/com/example/tugaspraktikum/TeksData2.kt
@@ -2,3 +2,10 @@ package com.example.TugasPraktikum
 
 
 
+
+@Composable
+fun FormDataDiriStyled(modifier: Modifier = Modifier) {
+    // states
+    var namaInput by remember { mutableStateOf("") }
+
+
